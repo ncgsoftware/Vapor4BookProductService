c7e57d928f2b1fe8f92a7bf75bb9162d500feb16
Jeremy Nelson <ncgsoftware@gmail.com>
2020-05-01 16:36:10 -0400

    postgre update

--- Sources/App/Configure.swift
+++ Sources/App/Configure.swift
@@ -1,5 +1,4 @@
 import Fluent
-//import FluentMySQLDriver
 import FluentPostgresDriver
 import Vapor
 import JWT
@@ -8,57 +7,42 @@ import JWT
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    /* Respond to browser CORS requests.
-        This middleware is answering the OPTIONS request by merely returning it positively without doing anything else. Browsers send an OPTIONS request to hosts that are different from the originating file. So, for example, if you run your frontend under https://www.domain.com and your API from https://api.domain.com, then the browsers will send out an OPTIONS request because www.domain.com is not equal to api.domain.com. There is nothing wrong with this, and this handy middleware takes care of it for us.
-    */
-    app.middleware.use(CORSMiddleware())
-    app.middleware.use(ErrorMiddleware() { request, error in
-        return Response(status: .internalServerError)
-    })
-
     
-    // Pass JWKS to our application that will contain the appropriate keys
     guard let jwksString = Environment.process.JWKS else {
-        fatalError("No value was found at the given public key enviroment 'JWKS'")}
-    
-
-    
-    // If there is a problem with the JWKS this will prevent the application from starting and therefore not expose any security issue.
-    try app.jwt.signers.use(jwksJSON: jwksString)
-    
-    
-    
-    /* MySQL Database
-    // Make sure we have a location for the database
-    guard let mysqlURL = Environment.process.MYSQL_CRED else {
-        fatalError("No value was found at the given public key enviroment")}
-    
-    // Make sure the database URL is actually a URL
-    guard let url = URL(string: mysqlURL) else {
-        fatalError("Cannot parse: \(mysqlURL) correctly.")
+        fatalError("No value was found at the given public key environment 'JWKS'")
     }
     
-    // Make sure the database is actually there
-    app.databases.use(try .mysql(url: url), as: .mysql)
-    */
+    guard let psqlUrl = Environment.process.PSQL_CRED else {
+        fatalError("No value was found at the given public key environment 'PSQL_CRED'")
+    }
     
-    
-    
-    // Postgre Database
-    guard let psqlUrl = Environment.process.PSQL_CRED else { fatalError("No value was found at the given public key environment 'PSQL_CRED'")
-           }
-    guard let url = URL(string: psqlUrl) else { fatalError("Cannot parse: \(psqlUrl) correctly.")
+    guard let url = URL(string: psqlUrl) else {
+        fatalError("Cannot parse: \(psqlUrl) correctly.")
     }
     
     app.databases.use(try .postgres(url: url), as: .psql)
+    app.middleware.use(CORSMiddleware())
+    app.middleware.use(ErrorMiddleware() { request, error in
+        struct ErrorResponse: Content {
+            var error: String
+        }
+        let data: Data?
+        do {
+            data = try JSONEncoder().encode(ErrorResponse(error: "\(error)"))
+        }
+        catch {
+            data = "{\"error\":\"\(error)\"}".data(using: .utf8)
+        }
+        let res = Response(status: .internalServerError, body: Response.Body(data: data!))
+        res.headers.replaceOrAdd(name: .contentType, value: "application/json")
+        return res
+    })
     
-
-    // Register database models for Fluent
+    try app.jwt.signers.use(jwksJSON: jwksString)
+    
     app.migrations.add(CreateCategory())
     app.migrations.add(CreateProduct())
-    
-    
+
     // register routes
     try routes(app)
 }
